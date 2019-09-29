73685146c5135c39e1c8b251a5b88f94d87f42aa
SpencerHowell <spencerhowell98@gmail.com>
2019-09-29 01:38:54 -0400

    Add website to server

--- server.py
+++ server.py
@@ -1,14 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 from alien_invasion.score_db import connect_and_collect, get_top_scores
 app = Flask(__name__)
-from flask import jsonify
-
 
 @app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/get_scores')
 def getscore():
     my_col = connect_and_collect()
     return jsonify(get_top_scores(my_col, 10))
 
-
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0", port=int("3000"), debug=True)                                                
