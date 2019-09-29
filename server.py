2aa54cde9118f4afde8173e2417f811d26fa2ecb
Manny Bhidya <abhidya@vols.utk.edu>
2019-09-29 01:08:01 -0400

    added server updated training

---
+++ server.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from alien_invasion.score_db import connect_and_collect, get_top_scores
+app = Flask(__name__)
+from flask import jsonify
+@app.route('/')
+def getscore():
+    my_col = connect_and_collect()
+    return jsonify(get_top_scores(my_col, 10))
+
+
+if __name__ == '__main__':
+    app.run()
